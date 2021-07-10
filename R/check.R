d6a04fdb4665fcf1189f4c577ab8eaf31bb20be4
Matt Dray <mwdray@gmail.com>
2021-07-10 12:39:02 +0100

    Break functions into utils, use tryCatch

--- R/check.R
+++ R/check.R
@@ -1,72 +1,23 @@
 
 #' Assess a Webpage for Linkrot
 #'
-#' Fetch the (https) URLs from a webpage and determine if they're still active
-#' by checking for a response (i.e. a response code of 200).
+#' Fetch web links from a given page and check for a response.
 #'
-#' @param page Character. A valid https URL to a webpage that contains links you
-#'     want to check for linkrot.
+#' @param page Character. A valid URL to a webpage that contains links you
+#'     want to check for linkrot. Must start 'http://www.' or 'https://www.'.
 #'
-#' @return A data.frame.
+#' @return A data.frame with 5 columns (page, link_url, link_text, response_code
+#'     and response_category, success) and as many rows per URLs found on the
+#'     page provided by the user.
+#'
 #' @export
 #'
-#' @examples \dontrun{lr_check("https://www.rostrum.blog/")}
+#' @examples \dontrun{ lr_check("https://www.rostrum.blog/") }
 lr_check <- function(page) {
 
-  if (class(page) != "character") {
-    stop("Argument 'page' must be a string.")
-  }
-
-  page_get <- httr::GET(page)
-
-  page_200 <- identical(httr::status_code(page_get), 200L)
-
-  if (!page_200) {
-
-    stop("That page couldn't be reached (i.e. the response code was not 200).")
-
-  } else {
-
-    # Fetch page content
-    page_html <- xml2::read_html(page)
-    link_nodes <- rvest::html_nodes(page_html, "a")
-    link_urls <- rvest::html_attr(link_nodes, "href")
-    link_texts <- rvest::html_text(link_nodes, "href")
-
-    # Compose links into table
-    links_df <- tibble::tibble(
-      page = page,
-      link_url = link_urls,
-      link_text = link_texts
-    )
-
-    # Filter for https links only
-    links_df <- links_df[grepl("^https", links_df$link_url), ]
-
-    # Get the status code
-    response_codes <- lapply(
-      links_df$link_url,
-      function(x) httr::status_code(httr::GET(x))
-    )
-
-    # Add column of codes and messages to table
-    links_df$response_code <- unlist(
-      lapply(
-        links_df$link_url,
-        function(x) httr::status_code(httr::GET(x))
-      )
-    )
-
-    # Convert status code to message
-    links_df$response_category <- unlist(
-      lapply(
-        links_df$response_code,
-        function(x) httr::http_status(x)$category
-      )
-    )
-
-    return(links_df)
-
-  }
+  .validate_page(page)
+  cat(paste0("[", Sys.time(), "]"), page)
+  links <- .fetch_links(page)
+  .check_links(links)
 
 }
---
+++ R/utils.R
@@ -0,0 +1,95 @@
+.validate_page <- function(page) {
+
+  if (class(page) != "character" | length(page) != 1) {
+    stop("Argument 'page' must be a character string of length 1.")
+  }
+
+  # Extremely basic URL validity check
+  is_url_ok <- grepl("^http://www\\.|^https://www\\.", page)
+
+  if (!is_url_ok) {
+    stop("Argument 'page' must begin 'http(s)://www.'.")
+  }
+
+  # Can page be reached?
+  pg_get    <- httr::GET(page)
+  pg_status <- httr::status_code(pg_get)
+  is_pg_ok  <- httr::http_error(pg_get)
+
+  if (is_pg_ok) {
+    stop("That page couldn't be reached (status code ", pg_status, ").")
+  }
+
+}
+
+.fetch_links <- function(page) {
+
+  # Fetch page content, extract links
+  page_html  <- xml2::read_html(page)
+  link_nodes <- rvest::html_nodes(page_html, "a")
+  link_urls  <- rvest::html_attr(link_nodes, "href")
+  link_texts <- rvest::html_text(link_nodes, "href")
+
+  # Compose links into table
+  links_df <- data.frame(
+    page = page,
+    link_url = link_urls,
+    link_text = link_texts
+  )
+
+  # Only the unique links
+  links_df <- unique(links_df)
+
+  # Only want links to webpages
+  pattern <- "^http://www\\.|^https://www\\."
+  links_df[grepl(pattern, links_df$link_url), ]
+
+}
+
+.check_links <- function(links_df) {
+
+  # Create response code column to fill with loop
+  links_df$response_code <- NA_real_
+
+  # Using a loop so we can return a dot for each iteration
+  for (i in 1:nrow(links_df)) {
+
+    tryCatch(
+      links_df$response_code[i] <-
+        httr::status_code(httr::GET(links_df$link_url[i])),
+      error = function(e) {
+        cat("!")
+        links_df$response_code[i] <- NA_real_
+      }
+    )
+
+    cat(".")
+
+  }
+
+  # Convert status code to message
+  links_df$response_category <- unlist(
+    lapply(
+      links_df$response_code,
+      function(x) {
+        tryCatch(
+          httr::http_status(x)$category,
+          error = function(e) NA_real_
+        )
+      }
+    )
+  )
+
+  # Logical rot status
+  links_df$success <- unlist(
+    lapply(
+      links_df$response_code,
+      function(x) ifelse(x != "Success", TRUE, FALSE)
+    )
+  )
+
+  cat("\n")
+
+  return(links_df)
+
+}
